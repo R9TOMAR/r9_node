fd4c149088ee47ec18bede02355d5f23efbb7313
rajeshwar <rajeshwar.clagtech@gmail.com>
2021-03-15 17:50:21 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res)=>{
+    res.send('rtomar');
+})
+
+app.listen('5000', (error)=>{
+    if(error)
+    console.log(error);
+    else
+    console.log('listening on port 5000');
+    
+})
